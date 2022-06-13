dad9caabb2fb3084a50e93e96007d9a233ce5369
thaian161 <thaian161@yahoo.com>
2022-06-13 18:54:44 -0500

    clean up example code from previous core work

--- express_server.js
+++ express_server.js
@@ -2,33 +2,14 @@ const express = require('express');
 const app = express();
 const PORT = 7070; // default port 8080
 const morgan = require('morgan');
+
 app.use(morgan('dev'));
+app.set('view engine', 'ejs');
 
 const urlDatabase = {
   b2xVn2: 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com',
 };
-
-app.get('/', (req, res) => {
-  res.send('Hello!');
-});
-
-app.get('/urls.json', (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>World</b></body></html>\n');
-});
-
-app.get('/set', (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
-});
-
-app.get('/fetch', (req, res) => {
-  res.send(`a = ${a}`);
-});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
