ccf8b6154a97e4fa72778dbde5d6146adf7f37d6
thaian161 <thaian161@yahoo.com>
2022-06-13 21:34:39 -0500

    add Post method for user to submit a form & add generateRandomString function

--- express_server.js
+++ express_server.js
@@ -2,19 +2,42 @@ const express = require('express');
 const app = express();
 const PORT = 7070; // default port 8080
 const morgan = require('morgan');
+//The body-parser library will convert the request body from a Buffer into string that we can read. It will then add the data to the req(request) object under the key body.
+const bodyParser = require('body-parser');
 
-app.use(morgan('dev'));
 app.set('view engine', 'ejs');
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const urlDatabase = {
   b2xVn2: 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com',
 };
 
+//Generate random 6 alphanumeric character to use for unique shortURL
+function generateRandomString() {
+  let characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  let charactersLength = characters.length;
+
+  for (let i = 0; i < 5; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+app.get('/', (req, res) => {
+  res.send('Welcome to TinyApp');
+});
+
 //adding route for /urls
 app.get('/urls', (req, res) => {
   const templateVars = { urlDatabaseObj: urlDatabase };
   res.render('urls_index', templateVars);
+});
+
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
 });
 
 //adding route for /urls/:shortURL
@@ -26,6 +49,11 @@ app.get('/urls/:shortURL', (req, res) => {
     longURL,
   };
   res.render('urls_show', templateVars);
+});
+
+app.post('/urls', (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send('Ok'); // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
