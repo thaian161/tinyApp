40a54be17f7e8d6c671963be96e8633aea633c0f
thaian161 <thaian161@yahoo.com>
2022-06-13 22:27:36 -0500

    redirect after submit form and redirect to the long URL page

--- express_server.js
+++ express_server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 7070; // default port 8080
+const PORT = 8080; // default port 8080
 const morgan = require('morgan');
 //The body-parser library will convert the request body from a Buffer into string that we can read. It will then add the data to the req(request) object under the key body.
 const bodyParser = require('body-parser');
@@ -8,11 +8,6 @@ const bodyParser = require('body-parser');
 app.set('view engine', 'ejs');
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
-
-const urlDatabase = {
-  b2xVn2: 'http://www.lighthouselabs.ca',
-  '9sm5xK': 'http://www.google.com',
-};
 
 //Generate random 6 alphanumeric character to use for unique shortURL
 function generateRandomString() {
@@ -26,13 +21,18 @@ function generateRandomString() {
   return result;
 }
 
+const urlDatabase = {
+  b2xVn2: 'http://www.lighthouselabs.ca',
+  '9sm5xK': 'http://www.google.com',
+};
+
 app.get('/', (req, res) => {
   res.send('Welcome to TinyApp');
 });
 
 //adding route for /urls
 app.get('/urls', (req, res) => {
-  const templateVars = { urlDatabaseObj: urlDatabase };
+  const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
 
@@ -53,7 +53,15 @@ app.get('/urls/:shortURL', (req, res) => {
 
 app.post('/urls', (req, res) => {
   console.log(req.body); // Log the POST request body to the console
-  res.send('Ok'); // Respond with 'Ok' (we will replace this)
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
+});
+
+app.get('/u/:shortURL', (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+
+  res.redirect(longURL);
 });
 
 app.listen(PORT, () => {
